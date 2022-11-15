d78e3d76a6529cf0a7850c052e7187ba8fd8bb86
zhaochencheng <zccbbg@qq.com>
2022-11-15 17:52:15 +0800

    增加ruoyi-mall 模块

---
+++ ruoyi-mall/src/test/java/com/example/ruoyimall/RuoyiMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ruoyimall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuoyiMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
