37153ca7c32ef87ae4f14a6ed3269997aab73f53
zhaochencheng <zccbbg@qq.com>
2022-11-16 14:47:44 +0800

    生成代码优化

---
+++ ruoyi-mall/src/main/java/com/example/ruoyimall/RuoyiMallApplication.java
@@ -0,0 +1,13 @@
+package com.example.ruoyimall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuoyiMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuoyiMallApplication.class, args);
+    }
+
+}
