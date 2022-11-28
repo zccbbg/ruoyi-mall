35158253f06f53d1fd6a4a79dc7fce4fdfd5d13a
zhaochencheng <zccbbg@qq.com>
2022-11-28 12:00:41 +0800

    新增微信表

--- sql/mall_ums结构.sql
+++ sql/mall_ums结构.sql
@@ -11,6 +11,10 @@ CREATE TABLE `ums_member` (
     `status` int(1) DEFAULT NULL COMMENT '帐号启用状态:0->禁用；1->启用',
     `avatar` varchar(256) DEFAULT NULL COMMENT '头像',
     `gender` int(1) DEFAULT NULL COMMENT '性别：0->未知；1->男；2->女',
+    `city` varchar(64) NULL COMMENT '用户所在城市',
+    `province` varchar(64) NULL COMMENT '用户所在省份',
+    `country` varchar(64) NULL COMMENT '用户所在国家',
+    `remark` varchar(256) NULL COMMENT '公众号运营者对粉丝的备注，公众号运营者可在微信公众平台用户管理界面对粉丝添加备注',
     `birthday` date DEFAULT NULL COMMENT '生日',
     `spread_uid` bigint unsigned DEFAULT '0' COMMENT '推广员id',
     `spread_time` datetime DEFAULT NULL COMMENT '推广员关联时间',
@@ -40,3 +44,24 @@ CREATE TABLE `ums_member_address` (
     `update_time` datetime(3) DEFAULT NULL COMMENT '修改时间',
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COMMENT='会员收货地址';
+
+DROP TABLE IF EXISTS `ums_wechat`;
+CREATE TABLE `ums_wechat` (
+    `id` bigint(20) NOT NULL AUTO_INCREMENT,
+    `member_id` bigint(20) DEFAULT NULL,
+    `unionid` varchar(30) NULL COMMENT '只有在用户将公众号绑定到微信开放平台帐号后，才会出现该字段',
+    `openid` varchar(30) NULL COMMENT '用户的标识，对当前公众号唯一',
+    `routine_openid` varchar(32) NULL COMMENT '小程序唯一身份ID',
+    `groupid` smallint(5) NULL DEFAULT '0' COMMENT '用户所在的分组ID（兼容旧的用户分组接口）',
+    `tagid_list` varchar(256) NULL COMMENT '用户被打上的标签ID列表',
+    `subscribe` tinyint(3) NULL DEFAULT '1' COMMENT '用户是否订阅该公众号标识',
+    `subscribe_time` int(10) NULL COMMENT '关注公众号时间',
+    `session_key` varchar(32) NULL COMMENT '小程序用户会话密匙',
+    access_token  varchar(500)                    null comment 'token',
+    expires_in    int                             null comment '过期时间',
+    refresh_token varchar(500)                    null comment '刷新token',
+    expire_time   datetime(3)                     null comment '过期时间',
+    `create_time` datetime(3) DEFAULT NULL COMMENT '创建时间',
+    `update_time` datetime(3) DEFAULT NULL COMMENT '修改时间',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COMMENT='用户微信信息';
